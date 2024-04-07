08996d52022b552c068251514ff2ae64d79bc4a0
Владислав Шварценберг <shvartsenberg2015@mail.ru>
2024-04-07 08:06:28 +0300

    task 3-1*

--- local/templates/my_home_template/header.php
+++ local/templates/my_home_template/header.php
@@ -37,7 +37,7 @@ use Bitrix\Main\Page\Asset;
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?=LANGUAGE_ID;?>">
 
 <head>
   <title><?$APPLICATION->ShowTitle()?></title>
@@ -173,106 +173,4 @@ use Bitrix\Main\Page\Asset;
         </div>
       </div>
     </div>
-  </div>
-
-  <div class="slide-one-item home-slider owl-carousel">
-  <?
-  $GLOBALS['arrFilterPriority'] = ["PROPERTY_PRIORITY_DEAL" => '5'];
-  $APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	".default", 
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "86400",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "N",
-		"COMPONENT_TEMPLATE" => ".default",
-		"DETAIL_URL" => "#IBLOCK_CODE#/#ELEMENT_CODE#/",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"FILTER_NAME" => "arrFilterPriority",
-		"USE_FILTER" => "Y",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "5",
-		"IBLOCK_TYPE" => "ads",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "10",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "PRIORITY_DEAL",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "Y",
-		"SET_META_KEYWORDS" => "Y",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "Y",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N"
-	),
-	false
-);
-  ?>
-
-    <div class="site-blocks-cover" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/hero_bg_1.jpg);" data-aos="fade"
-      data-stellar-background-ratio="0.5">
-
-      <div class="text">
-        <h2>853 S Lucerne Blvd</h2>
-        <p class="location"><span class="property-icon icon-room"></span> Los Angeles, CA 90005</p>
-        <p class="mb-2"><strong>$2,250,500</strong></p>
-
-
-        <p class="mb-0"><a href="#" class="text-uppercase small letter-spacing-1 font-weight-bold">More Details</a></p>
-
-      </div>
-    </div>
-
-    <div class="site-blocks-cover" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/hero_bg_3.jpg);" data-aos="fade"
-      data-stellar-background-ratio="0.5">
-
-      <div class="text">
-        <h2>625 S. Berendo St</h2>
-        <p class="location"><span class="property-icon icon-room"></span>607 Los Angeles, CA 90005</p>
-        <p class="mb-2"><strong>$2,250,500</strong></p>
-
-
-        <p class="mb-0"><a href="#" class="text-uppercase small letter-spacing-1 font-weight-bold">More Details</a></p>
-
-      </div>
-
-    </div>
-
   </div>
